4ef490fa3f9a59f2355735f7519c7c1525c18da4
nahshon5 <nahshonkyalo5@gmail.com>
2025-08-13 11:13:59 -0700

    Add week2assignment.py

---
+++ week2assignment.py
@@ -0,0 +1,18 @@
+my_list=[]
+print(my_list)
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+print(my_list)
+my_list.insert(1, 15)
+print(my_list)
+other_list=[50,60,70]
+my_list.extend(other_list)
+print(my_list)
+my_list.remove(70)
+print(my_list)
+my_list.sort()
+print(my_list)
+index_30=my_list.index(30)
+print(index_30)
